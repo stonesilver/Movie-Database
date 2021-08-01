2f76c228f0d0d0be9573e8b6bff59dfcdab36e93
Ezenwa Chigozie <stonesilver12@gmail.com>
2021-08-01 07:08:36 +0100

    fixed the height of the video card for mobile screens

--- src/components/known/knownForCard.component.jsx
+++ src/components/known/knownForCard.component.jsx
@@ -17,7 +17,7 @@ const KnownForCard = ({ name, title, poster_path, id }) => (
         <img
           src={
             poster_path
-              ? `https://image.tmdb.org/t/p/w185${poster_path}`
+              ? `https://image.tmdb.org/t/p/w780${poster_path}`
               : `https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-38-picture-grey-c2ebdbb057f2a7614185931650f8cee23fa137b93812ccb132b9df511df1cfac.svg`
           }
           alt={title ? title : name}

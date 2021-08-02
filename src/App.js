a37d5ac3319b45fe3bca689be6673d368ecedb05
Ezenwa Chigozie <stonesilver12@gmail.com>
2021-08-02 04:32:03 +0100

    bug fix v4

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Switch } from 'react-router-dom';
 import NavigationBar from './components/Navbar/Navbar';
 import Homepage from './pages/homepage/homepage.component';
-// import MoviePage from './pages/moviePage/moviePage.component';
-// import TvShowPage from './pages/tvShowPage/tvShowPage.component';
-// import PeoplePage from './pages/peoplePage/peoplePage.component';
-// import PageNotFound from './components/404Page/404Page.component';
+import MoviePage from './pages/moviePage/moviePage.component';
+import TvShowPage from './pages/tvShowPage/tvShowPage.component';
+import PeoplePage from './pages/peoplePage/peoplePage.component';
+import PageNotFound from './components/404Page/404Page.component';
 import Footer from './components/Footer/Footer';
-// import collectionDetails from './components/collection-page/collection-page.component';
-// import MyComponent from './components/lazy-load/lazy-load.component';
+import collectionDetails from './components/collection-page/collection-page.component';
+import MyComponent from './components/lazy-load/lazy-load.component';
 // import MovieImagesCarousel from './components/movieImagesCarousel/movieImagesCarousel.component';
-// import SearchPage from './components/searchPage/searchPage.component';
+import SearchPage from './components/searchPage/searchPage.component';
 // import YoutubeIframe from './components/youtube/youtube.component';
 // import { useLocation } from 'react-router-dom';
 import './App.scss';
@@ -28,17 +28,17 @@ const App = () => {
       <Switch>
         <Route exact path='/' component={Homepage} />
 
-        {/* <Route path='/movies' component={MoviePage} />
+         <Route path='/movies' component={MoviePage} />
         <Route path='/tv' component={TvShowPage} />
         <Route path='/people' component={PeoplePage} />
-        <Route
+        *<Route
           exact
           path='/collection/:collectionID'
           component={collectionDetails}
         />
         <Route exact path={`/search`} component={SearchPage} />
         <Route exact path='/lazy' component={MyComponent} />
-        <Route component={PageNotFound} /> */}
+        <Route component={PageNotFound} /> 
       </Switch>
       {/* {background && (
         <Route path={'/play/:youtubeID'} component={YoutubeIframe} />
